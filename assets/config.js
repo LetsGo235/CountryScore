2e9e7dffd5e84679814e865363b74c36484ff68e
LetsGo235 <141931285+LetsGo235@users.noreply.github.com>
2026-05-06 15:48:18 +0200

    Url Input

--- assets/config.js
+++ assets/config.js
@@ -2,6 +2,6 @@
 // Paste your deployed Apps Script Web App URL below after deployment.
 // You can also set it from the Setup page in the browser.
 window.COUNTRYSCORE_CONFIG = {
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzhqUTSxlfQ6TGvBMu_-4HLXOmKMp1vWy_JTqgW_gNWl7Kp-qgftjc9UxeIMlbiVOk/exec",
   REFRESH_SECONDS: 20
 };
